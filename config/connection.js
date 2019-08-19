7fa45e455af9373de57f3bffa5480653b568f826
Andrew <siricoa94@github.com>
2019-08-19 16:00:14 -0400

    Updated note taker to work on heroku hopefully

--- config/connection.js
+++ config/connection.js
@@ -1,22 +1,29 @@
-// Set up MySQL connection.
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "Adriana98!",
-  database: "notetaker_db"
-});
+var connection;
 
-// Make connection.
+if (process.env.JAWSDB_URL) {
+   connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "Adriana98!",
+    database: "notetaker_db"
+  });
+}
+
 connection.connect(function(err) {
-  if (err) {
-    console.error("error connecting: " + err.stack);
-    return;
-  }
+  if (err) throw err;
   console.log("connected as id " + connection.threadId);
 });
 
-// Export connection for our ORM to use.
+connection.config.typeCast = function(field, next) {
+  if (field.type == "TINY" && field.length == 1) {
+    return field.string() == "1"; // 1 = true, 0 = false
+  }
+  return next();
+};
+
 module.exports = connection;
